008120d1dd301847a2bbeef1cca896f6b527c762
sujana25 <160763944+sujana25@users.noreply.github.com>
2024-06-18 22:09:11 +0530

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,6 @@
+import time_series_visualizer
+
+# Test the functions and create the plots
+time_series_visualizer.draw_line_plot()
+time_series_visualizer.draw_bar_plot()
+time_series_visualizer.draw_box_plot()
